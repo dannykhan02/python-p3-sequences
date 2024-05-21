cee59cac0c05c43e3cef050fc953a8cb49916029
dannykhan02 <dannykhan614@gmail.com>
2024-05-21 20:28:43 +0300

    create my file

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fib = [0, 1]
+    while len(fib) < length:
+        fib.append(fib[-1] + fib[-2])
+    print(fib[:length])
+
+print_fibonacci(9)
+   
